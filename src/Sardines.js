744485e98a8c249a27468ecd3e0edc69500b8383
Connor Ashley <ashleycm@vcu.edu>
2021-07-08 16:01:54 -0400

    prepared app for development and created most basic components of vending machine

---
+++ src/Sardines.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Sardines.css';
+
+class Sardines extends Component {
+    render() {
+        return(
+            <div className='Sardines'>
+                <h1>Sardine Eat You!</h1>
+            </div>
+        )
+    }
+}
+
+export default Sardines;
