40caddb71c2f16a8dd48bb2f2b1914b2254b9640
Connor Ashley <ashleycm@vcu.edu>
2021-07-08 16:09:20 -0400

    implemented basic routes for vending machine components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import {Route, Switch, NavLink} from 'react-router-dom';
 import Soda from './Soda';
 import Chips from './Chips';
 import Sardines from './Sardines';
+import VendingMachine from './VendingMachine';
 
 function App() {
   return (
     <div className="App">
-      <Soda />
-      <Chips />
-      <Sardines />
+      <Switch>
+        <Route exact path ='/' component={VendingMachine} />
+        <Route exact path ='/soda' component={Soda} />
+        <Route exact path ='/chips' component={Chips} />
+        <Route exact path ='/sardines' component={Sardines} />
+      </Switch>
     </div>
   );
 }
