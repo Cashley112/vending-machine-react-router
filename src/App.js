05091caf37e00cbeb73d7a9725e45cd0e7c4e4f6
Connor Ashley <ashleycm@vcu.edu>
2021-07-08 16:16:19 -0400

    added in navlinks

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import VendingMachine from './VendingMachine';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink exact activeClassName='active-link' to="/">VendingMachine</NavLink>
+        <NavLink exact activeClassName='active-link' to="/soda">Soda</NavLink>
+        <NavLink exact activeClassName='active-link' to="/chips">Chips</NavLink>
+        <NavLink exact activeClassName='active-link' to="/sardines">Sardines</NavLink>
+      </nav>
       <Switch>
         <Route exact path ='/' component={VendingMachine} />
         <Route exact path ='/soda' component={Soda} />
