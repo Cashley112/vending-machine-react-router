d9e121102c5c4682b02c939d5ba80dcf8eabfd8d
Connor Ashley <ashleycm@vcu.edu>
2021-07-08 16:08:59 -0400

    created most basic instance of VendingMachine comp

---
+++ src/VendingMachine.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './VendingMachine.css';
+
+class VendingMachine extends Component {
+    render() {
+        return(
+            <div className='VendingMachine'>
+                <h1>This is Vending Machine</h1>
+            </div>
+        )
+    }
+}
+
+export default VendingMachine;
